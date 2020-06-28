a095a82b87b7316c268a4bc4d82140dd92cdf1c4
Nadia Pougatcheva <nadia_pougatcheva@epam.com>
2020-06-28 22:51:22 +0300

    feature: extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,18 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(initStr, opts = {}) {
+  if (initStr === undefined) {
+      throw new Error() 
+    }
+    const options = {...opts}
+    options.addition = options.addition===undefined ? '' : options.addition;
+    
+    const str = `${initStr}`;
+    let addVal = `${options.addition}`;
+    
+    const addArr = Array(options.additionRepeatTimes).fill(addVal);
+    
+    const additionSeparator = options.additionSeparator !== undefined ? options.additionSeparator : '|'
+    const addition = addArr.join(additionSeparator) ;
+    
+    const arr = Array(options.repeatTimes).fill(str).map(item => item + addition);
+    return arr.join(options.separator || '+');
 };
-  
