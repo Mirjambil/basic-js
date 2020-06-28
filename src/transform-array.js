3ff39757f435ea2ba056a3a2f155db5985e0d091
Nadia Pougatcheva <nadia_pougatcheva@epam.com>
2020-06-28 20:12:01 +0300

    feature: transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,58 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function transform(initArr) {
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+  if (!Array.isArray(initArr)) {
+    throw new Error();
+  }
+
+  const arr = [...initArr];
+  const result = []
+
+  arr.forEach ((item, i, arr) => {
+    
+    switch (item) {
+      case 'REMOVED' :
+        break;
+      case 'DOUBLED' :
+        break;
+      case '--discard-prev' : {
+        if (!result.length || !i) {
+          break;
+        }
+        const prev = arr[i-1]
+        if (prev !=='REMOVED') {
+          result.pop();
+          arr[i-1] = 'REMOVED'
+        }
+        break;
+      }
+      case '--double-prev' : {
+        if (!result.length || !i) {
+          break;
+        }
+        const prev = arr[i-1]
+        if (prev !== 'REMOVED') {
+          result.push(prev)
+        }
+        break;
+      }
+      case '--discard-next' : {
+        if (i < arr.length-1) {
+          arr[i+1] = 'REMOVED'
+        }
+        break;
+      }
+      case '--double-next' : {
+        if (i < arr.length-1) {
+          const next = arr[i+1]
+          result.push(next)
+        }
+        break;
+      }
+      default: {
+        result.push(item);
+      }
+    }
+  })
+
+  return result;
+}
