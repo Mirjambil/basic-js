3eb8109b59802d48bbb7f3bfb6926a99e12ef9ea
Nadia Pougatcheva <nadia_pougatcheva@epam.com>
2020-06-28 21:34:20 +0300

    feature: calculate hanoi tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,14 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+const secInHour = 3600; // 60 * 60;
+module.exports = function calculateHanoi(disksNumber, turnsSpeed ) {
+  if (isNaN(disksNumber) || isNaN(turnsSpeed)) {
+    throw new Error()
+  }
+  const turns = Math.pow(2, disksNumber) - 1
+  const hours = (turns / turnsSpeed )
+  const seconds = Math.floor(hours *  secInHour)
+  const res = {
+    turns,
+    seconds
+  };
+  return res;
 };
